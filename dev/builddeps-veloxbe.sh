6cee9803cc95f15ca9367ede2d5da6e6e23b415b
Rong Ma <marong@apache.org>
2025-08-25 15:42:16 +0100

    das support for s3,abfs,gcs
    add python script for ut
    
    remove scala ut
    
    Alchemy-item: (ID = 1742) das support for s3,abfs,gcs commit 1/3 - 27208b11fdeea1f35441725de28d52cad71e230c
    
    Alchemy-item: (ID = 1805) das support for s3,abfs,gcs commit 1/4 - 8a0b02ce9dfd41ef6bbe7b374405b7eb4e31f880

--- dev/builddeps-veloxbe.sh
+++ dev/builddeps-veloxbe.sh
@@ -39,6 +39,7 @@ ENABLE_ABFS=OFF
 ENABLE_VCPKG=OFF
 ENABLE_GPU=OFF
 ENABLE_ENHANCED_FEATURES=OFF
+ENABLE_DAS=OFF
 RUN_SETUP_SCRIPT=ON
 VELOX_REPO=""
 VELOX_BRANCH=""
@@ -113,6 +114,10 @@ do
         ;;
         --enable_enhanced_features=*)
         ENABLE_ENHANCED_FEATURES=("${arg#*=}")
+        shift # Remove argument name from processing
+        ;;
+        --enable_das=*)
+        ENABLE_DAS=("${arg#*=}")
         shift # Remove argument name from processing
         ;;
         --run_setup_script=*)
@@ -270,6 +275,7 @@ function build_gluten_cpp {
     "-DENABLE_GPU=$ENABLE_GPU"
     "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
     "-DENABLE_ENHANCED_FEATURES=$ENABLE_ENHANCED_FEATURES"
+    "-DENABLE_DAS=$ENABLE_DAS"
   )
 
   if [ -n "${INSTALL_PREFIX:-}" ]; then
